fc76654075ac6045db7fc3c9df2c3ebf617e0ccb
sysatom <sysatom@gmail.com>
2020-10-22 15:58:57 +0800

    feat: proto

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+
+	pb "github.com/sysatom/microservices/proto/consignment"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+type Repository struct {
+	mu           sync.Mutex
+	consignments []*pb.Consignment
+}
+
+func (r *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	updated := append(r.consignments, consignment)
+	r.consignments = updated
+	return consignment, nil
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
